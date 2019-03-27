ae7b556ef846363aa6304ab3cfeda316992631c3
Jorik Kraaikamp <jorikkraaikamp@gmail.com>
2019-03-27 20:14:18 +0100

    Worked a bit on the tests and started on the integration with the api

--- src/drc/conf/base.py
+++ src/drc/conf/base.py
@@ -246,6 +246,14 @@ LOGGING = {
             'maxBytes': 1024 * 1024 * 10,  # 10 MB
             'backupCount': 10
         },
+        'cmis': {
+            'level': 'DEBUG',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': os.path.join(LOGGING_DIR, 'cmis.log'),
+            'formatter': 'verbose',
+            'maxBytes': 1024 * 1024 * 10,  # 10 MB
+            'backupCount': 10
+        },
         'performance': {
             'level': 'INFO',
             'class': 'logging.handlers.RotatingFileHandler',
@@ -265,6 +273,11 @@ LOGGING = {
             'handlers': ['project'],
             'level': 'INFO',
             'propagate': True,
+        },
+        'drc.cmis': {
+            'handlers': ['cmis'],
+            'level': 'INFO',
+            'propagate': False,
         },
         'django.request': {
             'handlers': ['django'],
